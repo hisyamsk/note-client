9f858a7631d37639ea5ee53468d3cda643a5c635
Hisyam <hisyamskurniawan18@gmail.com>
2023-02-10 18:39:51 +0700

    chore: tidy up code

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Layout from './scenes/Layout';
 import Login from './features/auth/Login';
 import Public from './scenes/Public';
 import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
 
 function App() {
   return (
@@ -14,6 +16,14 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="dash" element={<DashLayout />}>
             <Route index element={<Welcome />} />
+
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+            </Route>
+
+            <Route path="users">
+              <Route index element={<UsersList />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
