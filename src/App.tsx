6c998c7ae1acff212c87d92e72d519c4d6fcacbe
Hisyam <hisyamskurniawan18@gmail.com>
2023-03-28 21:07:18 +0700

    feat: add prefetch component for manual subscription

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import EditNote from './features/notes/EditNote';
 import NewNote from './features/notes/NewNote';
 import EditUser from './features/users/EditUser';
 import NewUserForm from './features/users/NewUserForm';
+import Prefetch from './features/auth/Prefetch';
 
 function App() {
   return (
@@ -18,19 +19,22 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Public />} />
           <Route path="login" element={<Login />} />
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
 
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path=":id" element={<EditNote />} />
-              <Route path="new" element={<NewNote />} />
-            </Route>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=":id" element={<EditUser />} />
-              <Route path="new" element={<NewUserForm />} />
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
+
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
             </Route>
           </Route>
         </Route>
