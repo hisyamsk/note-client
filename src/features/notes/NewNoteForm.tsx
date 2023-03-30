41c8f98de9cac269291e7f5eb39bc1b07c629313
Hisyam <hisyamskurniawan18@gmail.com>
2023-03-31 00:45:44 +0700

    feat: add Add Note component

--- src/features/notes/NewNoteForm.tsx
+++ src/features/notes/NewNoteForm.tsx
@@ -1,7 +1,124 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IUsersResponse } from '../../interface/response.interface';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useAddNewNoteMutation } from './notesApiSlice';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
 
 const NewNoteForm = ({ users }: { users: IUsersResponse[] }): JSX.Element => {
-  return <div>NewNoteForm</div>;
+  const navigate = useNavigate();
+  const [addNewNote, { isLoading, isSuccess, isError, error }] =
+    useAddNewNoteMutation();
+
+  const [title, setTitle] = useState<string>('');
+  const [text, setText] = useState<string>('');
+  const [userId, setUserId] = useState<string>(users[0].id);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setTitle('');
+      setText('');
+      setUserId('');
+      navigate('/dash/notes');
+    }
+  }, [isSuccess, navigate]);
+
+  const onTitleChanged = (e: React.FormEvent<HTMLInputElement>) =>
+    setTitle(e.currentTarget.value);
+  const onTextChanged = (e: React.FormEvent<HTMLTextAreaElement>) =>
+    setText(e.currentTarget.value);
+  const onUserIdChanged = (e: React.FormEvent<HTMLSelectElement>) =>
+    setUserId(e.currentTarget.value);
+
+  const canSave = [title, text, userId].every(Boolean) && !isLoading;
+
+  const onSaveNoteClicked = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (canSave) {
+      await addNewNote({ user: userId, title, text });
+    }
+  };
+
+  const renderErrorMessage = () => {
+    if (isError && error) {
+      if ('status' in error) {
+        const errMsg =
+          'error' in error ? error.error : JSON.stringify(error.data);
+
+        return errMsg;
+      } else {
+        return error.message;
+      }
+    }
+    return '';
+  };
+
+  const renderOptions = () => {
+    return users.map((user) => (
+      <option key={user.id} value={user.id}>
+        {user.username}
+      </option>
+    ));
+  };
+
+  const errClass = isError ? 'errmsg' : 'offscreen';
+  const validTitleClass = !title ? 'form__input--incomplete' : '';
+  const validTextClass = !text ? 'form__input--incomplete' : '';
+
+  return (
+    <>
+      <p className={errClass}>{renderErrorMessage()}</p>
+      <form className="form" onSubmit={onSaveNoteClicked}>
+        <div className="form__title-row">
+          <h2>New Note</h2>
+          <div className="form__action-buttons">
+            <button className="icon-button" title="Save" disabled={!canSave}>
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="title">
+          Title:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="title"
+          type="text"
+          autoComplete="off"
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label className="form__label" htmlFor="text">
+          Text:
+        </label>
+        <textarea
+          className={`form__input form__input--text ${validTextClass}`}
+          id="text"
+          name="text"
+          value={text}
+          onChange={onTextChanged}
+        />
+
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="username"
+        >
+          ASSIGNED TO:
+        </label>
+        <select
+          id="username"
+          name="username"
+          className="form__select"
+          value={userId}
+          onChange={onUserIdChanged}
+        >
+          {renderOptions()}
+        </select>
+      </form>
+    </>
+  );
 };
 
 export default NewNoteForm;
